04fdd91053737558443df2c624dde3885382c896
Joao Humberto Tecila Mayer <joao_h_mayer@estudante.sesisenai.org.br>
2025-09-10 14:15:54 -0300

    botei para quando rodar o servidor aparecer o link que abre o index

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Lê o JSON local
+app.use(express.static(path.join(__dirname, 'frontend')));
+
 let pokemon = JSON.parse(fs.readFileSync('pokemons.json'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+});
 
 app.get('/pokemon', (req, res) => {
   res.json(pokemon);
 });
 
 app.listen(8000, () => {
-  console.log('Servidor rodando em http://localhost:8000');
+  console.log('Site rodando em http://localhost:5500/frontend');
 });
