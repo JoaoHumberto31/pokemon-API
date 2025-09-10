ae9be09ce1b3fee78c098268a9b86206724fa538
Joao Humberto Tecila Mayer <joao_h_mayer@estudante.sesisenai.org.br>
2025-09-10 16:31:46 -0300

    tirei aquela bomba

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const path = require('path')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'frontend')));
 
 let pokemon = JSON.parse(fs.readFileSync('pokemons.json'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
-});
 
 app.get('/pokemon', (req, res) => {
   res.json(pokemon);
 });
 
 app.listen(8000, () => {
-  console.log('Site rodando em http://localhost:5500/frontend');
+  console.log('Site rodando em http://localhost:8000');
 });
